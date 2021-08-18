170aa48f15e81cf115539b2e1649ea5d954798e0
Anders Hefre <Anders.Hefre@skatteetaten.no>
2021-08-18 20:45:09 +0200

    Fikset et par tester slik at prinsippet med å bruke set<locale> for holidays funker.

--- src/main/java/no/skatteetaten/domain/WorkdayCalendar.java
+++ src/main/java/no/skatteetaten/domain/WorkdayCalendar.java
@@ -1,4 +1,71 @@
 package no.skatteetaten.domain;
 
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.THURSDAY;
+import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.WEDNESDAY;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class WorkdayCalendar {
+
+    private final Set<LocalDate> holidays = new HashSet<>();
+
+    public Date getWorkdayIncrement(Date startDate, float incrementInWorkdays) {
+        LocalDate startDato = LocalDate.of(2012, 3, 7);
+        LocalDate endDate = LocalDate.of(2012, 6, 7);
+
+// I've hardcoded the holidays as LocalDates
+// and put them in a Set
+// For the sake of efficiency, I also put the business days into a Set.
+// In general, a Set has a better lookup speed than a List.
+        final Set<DayOfWeek> businessDays = Set.of(
+            MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+        );
+
+        List<LocalDate> allDates =
+
+            // Java 9 provides a method to return a stream with dates from the
+            // startdate to the given end date. Note that the end date itself is
+            // NOT included.
+            startDato.datesUntil(endDate)
+
+                // Retain all business days. Use static imports from
+                // java.time.DayOfWeek.*
+                .filter(t -> businessDays.contains(t.getDayOfWeek()))
+
+                // Retain only dates not present in our holidays list
+                .filter(t -> !holidays.contains(t))
+
+                // Collect them into a List. If you only need to know the number of
+                // dates, you can also use .count()
+                .collect(Collectors.toList());
+
+        return new Date();
+    }
+
+    public void setWorkdayStartAndStop(Calendar Start, Calendar stop) {
+
+    }
+
+    public void setRecurringHoliday(GregorianCalendar gregorianCalendar) {
+        holidays.add(gregorianCalendar.toZonedDateTime().toLocalDate());
+    }
+
+    public void setHoliday(GregorianCalendar gregorianCalendar) {
+
+    }
+
+    public Set<LocalDate> getHolidays() {
+        return holidays;
+    }
 }
--- src/test/java/no/skatteetaten/domain/WorkdayCalendarTest.java
+++ src/test/java/no/skatteetaten/domain/WorkdayCalendarTest.java
@@ -1,14 +1,60 @@
 package no.skatteetaten.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WorkdayCalendarTest {
+    private WorkdayCalendar workdayCalendar;
+
+    @BeforeEach
+    public void oppsett() {
+        workdayCalendar = new WorkdayCalendar();
+    }
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void test_that_holidays_returns_correct_number() {
+        GregorianCalendar cal = new GregorianCalendar(2021, 11, 31);
+        workdayCalendar.setRecurringHoliday(cal);
+        assertTrue(workdayCalendar.getHolidays().contains(LocalDate.of(2021, 12, 31)));
+    }
+
+    @Test
+    public void test_that_calculator_shows_correct_() {
+        workdayCalendar.setWorkdayStartAndStop(
+            new GregorianCalendar(2004, Calendar.JANUARY, 1, 8, 0),
+            new GregorianCalendar(2004, Calendar.JANUARY, 1, 16, 0));
+
+        workdayCalendar.setRecurringHoliday(
+            new GregorianCalendar(2004, Calendar.MAY, 17, 0, 0));
+
+        workdayCalendar.setHoliday(
+            new GregorianCalendar(2004, Calendar.MAY, 27, 0, 0));
+        SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        Date start = new GregorianCalendar(2004, Calendar.MAY, 24, 18, 5).getTime();
+        float increment = -5.5f;
+        System.out.println(
+            f.format(start) + " med tillegg av " +
+                increment + " arbeidsdager er " + f.format(workdayCalendar.getWorkdayIncrement(start, increment)));
+
+        assertEquals(workdayCalendar.getWorkdayIncrement(start, increment),
+            formatOutput(f, start, increment));
+    }
+
+    private String formatOutput(SimpleDateFormat f, Date start, Float increment) {
+        return f.format(start) + " med tillegg av " +
+            increment + " arbeidsdager er " + f.format(workdayCalendar.getWorkdayIncrement(start, increment));
+
     }
 
 }
